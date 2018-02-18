efa3c3a873fd5003a2221b9a2ea673cc097f889a
fnoop <fnoopdogg@gmail.com>
2018-02-18 10:45:18 +0000

    Fix HMR, #17

--- build/dev-server.js
+++ build/dev-server.js
@@ -36,12 +36,14 @@ const hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: false
 })
 // force page reload when html-webpack-plugin template changes
+/*
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({ action: 'reload' })
     cb()
   })
 })
+*/
 
 // enable hot-reload and state-preserving
 // compilation error display
