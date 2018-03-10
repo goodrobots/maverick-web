4c8b5e4650d23137b73ee7233c28a39ca27230de
fnoop <fnoopdogg@gmail.com>
2018-03-10 22:32:32 +0000

    Fix prod build static path, #47

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '',
     productionSourceMap: true,
     productionGzip: true,
