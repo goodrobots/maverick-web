c9efa35de04324cfa08e922b117e24fbffa548b2
fnoop <fnoopdogg@gmail.com>
2018-04-02 13:04:09 +0100

    Add tooltips to navfab, fixes #66

--- src/components/common/ActionButton.vue
+++ src/components/common/ActionButton.vue
@@ -5,14 +5,22 @@ v-card.transparent.navfab
     v-btn(slot="activator" :color="navColor" dark fab hover v-model="navfab")
       v-icon(v-html="navIcon")
       v-icon close
-    v-btn(v-if="(moduleName !== 'analysis')" fab dark :small="(moduleName !== 'analysis')" color="mavgreen" to="/analysis")
-      v-icon equalizer
-    v-btn(v-if="(moduleName !== 'config')" fab dark :small="(moduleName !== 'config')" color="mavpurple" to="/config")
-      v-icon settings
-    v-btn(v-if="(moduleName !== 'planner')" fab dark :small="(moduleName !== 'planner')" color="mavorange" to="/planner")
-      v-icon explore
-    v-btn(v-if="(moduleName !== 'cockpit')" fab dark :small="(moduleName !== 'cockpit')" color="mavblue" to="/cockpit")
-      v-icon flight_takeoff
+    v-tooltip(v-if="(moduleName !== 'analysis')" left)
+      v-btn( fab dark :small="(moduleName !== 'analysis')" color="mavgreen" to="/analysis" slot="activator")
+        v-icon equalizer
+      span Analysis
+    v-tooltip(v-if="(moduleName !== 'config')" left)
+      v-btn(fab dark :small="(moduleName !== 'config')" color="mavpurple" to="/config" slot="activator")
+        v-icon settings
+      span Configuration
+    v-tooltip(v-if="(moduleName !== 'planner')" left)
+      v-btn(fab dark :small="(moduleName !== 'planner')" color="mavorange" to="/planner" slot="activator")
+        v-icon explore
+      span Planner
+    v-tooltip(v-if="(moduleName !== 'cockpit')" left)
+      v-btn(fab dark :small="(moduleName !== 'cockpit')" color="mavblue" to="/cockpit" slot="activator")
+        v-icon flight_takeoff
+      span Cockpit
     // v-divider
     // v-btn(fab dark small :color="(navState) ? navColor : 'grey'" @click="toggleNavState")
       v-icon(v-if="navState") swap_vert
@@ -21,17 +29,20 @@ v-card.transparent.navfab
       v-icon(v-if="fullScreen") fullscreen
       v-icon(v-else) fullscreen_exit
 
-  v-speed-dial.navfab(v-model="navfab" direction="left" bottom=true right=true open-on-hover=false transition="slide-y-reverse-transition")
+  v-speed-dial.navfab(v-model="navfab" direction="left" bottom=true right=true open-on-hover=false transition="slide-x-reverse-transition")
     v-btn(slot="activator" :color="navColor" dark fab hover v-model="navfab")
       v-icon(v-html="navIcon")
       v-icon close
-    v-btn(fab dark small :color="(navState) ? navColor : 'grey'" @click="toggleNavState")
-      v-icon(v-if="navState") swap_vert
-      v-icon(v-else) swap_horiz
-    v-btn(fab dark small :color="(fullScreen) ? navColor : 'grey'" @click="toggleFullScreen")
-      v-icon(v-if="fullScreen") fullscreen
-      v-icon(v-else) fullscreen_exit
-
+    v-tooltip(top)
+      v-btn(fab dark small :color="(navState) ? navColor : 'grey'" @click="toggleNavState" slot="activator")
+        v-icon(v-if="navState") swap_vert
+        v-icon(v-else) swap_horiz
+      span Nav
+    v-tooltip(top)
+      v-btn(fab dark small :color="(fullScreen) ? navColor : 'grey'" @click="toggleFullScreen" slot="activator")
+        v-icon(v-if="fullScreen") fullscreen
+        v-icon(v-else) fullscreen_exit
+      span Fullscreen
 </template>
 
 <script>
@@ -70,14 +81,15 @@ export default {
       })
     }
   }
+
 }
 </script>
 
 <style>
   .navfab .speed-dial {
     position: absolute;
-    bottom: 25px;
-    right: 25px;
+    bottom: 40px;
+    right: 40px;
   }
 
   .navfab .btn--floating {
