a64137124ccdca8a4ecbfe9b52dd619c58e7b510
fnoop <fnoopdogg@gmail.com>
2019-02-05 23:44:26 +0000

    Suppress yarn install warnings, update packages #118

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue/recommended', '@vue/standard'],
+  extends: ['plugin:vue/recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
