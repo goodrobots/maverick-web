05b611d4adb9eaf725d3777826f7595ab9a7c2da
fnoop <fnoopdogg@gmail.com>
2019-02-07 13:53:00 +0000

    Disable temporary logos

--- src/components/modules/home/HomeModule.vue
+++ src/components/modules/home/HomeModule.vue
@@ -5,7 +5,7 @@ v-content
       v-flex(xs12 sm6 md6)
         v-card.transparent.elevation-0
           <v-responsive></v-responsive>
-            img(src='@/assets/img/logos/maverick-text-logo-dark.svg' height='85%' width='75%')
+            // img(src='@/assets/img/logos/maverick-text-logo-dark.svg' height='85%' width='75%')
         v-container
           v-layout(row wrap justify-center)
             v-flex(xs6 sm6 md6 text-xs-center)
@@ -22,7 +22,7 @@ v-content
                 v-icon(right) equalizer
         v-footer.transparent(absolute height="auto")
           v-layout.pb-5(row wrap justify-center)
-            img(src="@/assets/img/logos/goodrobots-text-logo-dark.svg" height='25%')
+            // img(src="@/assets/img/logos/goodrobots-text-logo-dark.svg" height='25%')
 </template>
 
 <script>
