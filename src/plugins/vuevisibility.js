b386d2be4fd421d0b02c666ace34b6535d551ac1
fnoop <fnoopdogg@gmail.com>
2019-02-16 08:26:34 +0000

    Rename visibility to VueVisibility

--- src/plugins/vuevisibility.js
+++ src/plugins/vuevisibility.js
@@ -1,12 +1,12 @@
 
 import Vue from 'vue'
-import visibility from 'vue-visibility-change'
+import VueVisibility from 'vue-visibility-change'
 import store from '../store'
 
 // Activate global directives and methods
-Vue.use(visibility)
+Vue.use(VueVisibility)
 
 // Add a handler to update vuex state when page becomes visible/invisible
-const handler = visibility.change((event, hidden) => {
+const handler = VueVisibility.change((event, hidden) => {
   store.commit('setAppVisible', !hidden)
 });
