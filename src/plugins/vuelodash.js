2527d709cfa7e1a1a99cc9b26c6da4f679b3fc2d
fnoop <fnoopdogg@gmail.com>
2020-02-27 12:44:09 +0000

    Fixup new vue-lodash syntax

--- src/plugins/vuelodash.js
+++ src/plugins/vuelodash.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueLodash from 'vue-lodash'
+import lodash from 'lodash'
 
 // Activate lodash under standard naming - this._.
-Vue.use(VueLodash)
+Vue.use(VueLodash, { lodash: lodash })
+
