1f6757eb53058e031eba7258b1c6866ce9161d5c
fnoop <fnoopdogg@gmail.com>
2020-03-05 15:24:05 +0000

    Refactor video module to use new video stream definitions #147

--- src/components/modules/video/VideoModule.vue
+++ src/components/modules/video/VideoModule.vue
@@ -1,15 +1,30 @@
 <template lang='pug'>
 v-content
-  v-container(fluid grid-list-xl fill-height)
+  v-container
+    v-card.mx-auto
+      v-toolbar(:color="navColor" dense)
+        v-toolbar-title Video Streams
+      v-item-group
+        v-container
+          v-row
+            v-col(v-for="stream in videostreams" :key="stream.key" cols="12" md="6")
+              v-item(v-slot:default="{ active, toggle }")
+                v-card.d-flex.align-center(:color="active ? navColor : ''" height="100" @click="changeStream(stream)")
+                  v-scroll-y-transition
+                    div.display-2.flex-grow-1.text-center(v-if="active")
+                      div {{ stream.name }}
+                      div Active
+                    div.display-1.flex-grow-1.text-center(v-else)
+                      div {{ stream.name }}
+          v-alert(v-if="!Object.keys(videostreams).length" outlined border="left" type="warning")
+            span No Video Streams are defined.  Please define a video stream in Config->Video
+  v-container(fluid grid-list-xl)
     v-layout(align-center justify-space-around column)
       v-flex(xs12)
-        v-spacer
-          v-content
-      v-flex(xs12)
         v-layout(justify-center wrap)
-          v-card()
+          v-card(v-if='activeStream')
             v-system-bar(:color="navColor" window)
-              span Realtime Video Stream
+              span Video Stream: {{ activeStream.name }}
             v-card-text
               v-chip.ma-2(v-if="streamStatus=='playing'" color='green')
                 v-icon(left) mdi-check-circle
@@ -25,7 +40,7 @@ v-content
               span Packet Loss
                 v-chip.ma-2(color='red') <strong>{{ statPacketloss }}</strong>
             v-container
-              VideoStream(:config="{ url: 'wss://maverick-raspberry.local:6796' }" :width="width" :stream="1" @status="streamStatus = $event" @bitrate="statBitrate = $event" @packetloss="statPacketloss = $event")
+              VideoStream(:config="{ url: activeStream.webrtcEndpoint }" :key="activeStream.key" :width="width" :stream="1" @status="streamStatus = $event" @bitrate="statBitrate = $event" @packetloss="statPacketloss = $event")
             v-card-text
               v-layout.d-flex.justify-space-between(flat)
                 div
@@ -36,7 +51,7 @@ v-content
                     v-btn(color='red' value='stop')
                       v-icon(left) mdi-stop
                       span Stop
-                div
+                // div
                   span BitRate
                   v-btn-toggle.ma-2(v-model="bitrate" mandatory background-color='blue' active-class='blue' dense)
                     v-btn(value='128') 128K
@@ -45,7 +60,7 @@ v-content
                     v-btn(value='1024') 1M
                     v-btn(value='2048') 2M
 
-                div
+                // div
                   span Resolution
                   v-btn-toggle.ma-2(v-model="resolution" mandatory background-color='purple' active-class='purple' dense)
                     v-btn(value='480p') 480p
@@ -53,9 +68,6 @@ v-content
                     v-btn(value='1080p') 1080p
                     v-btn(value='2k') 2k
                     v-btn(value='4k') 4k
-      v-flex(xs12)
-        v-spacer
-          v-content
 </template>
 
 <script>
@@ -74,6 +86,7 @@ export default {
       statPacketloss: null,
       bitrate: '256',
       resolution: '720p',
+      activeStream: null,
       width: ((75 / 100) * window.innerWidth)
     }
   },
@@ -84,12 +97,22 @@ export default {
       } else {
         return Math.round(this.statBitrate / 1000000) + ' M'
       }
+    },
+    videostreams () {
+      return this.$store.state.data.videostreams
     }
+  },
+  watch: {
   },
   mounted () {
     window.addEventListener('resize', () => {
       this.width = ((75 / 100) * window.innerWidth)
     })
+  },
+  methods: {
+    changeStream (stream) {
+      this.activeStream = stream
+    }
   }
 }
 </script>
