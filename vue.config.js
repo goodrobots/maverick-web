55c3a84a21eaa30d4228ad7073a89c8a33b2eb51
Sam Dudley <dudley.samuel@gmail.com>
2020-03-18 15:30:46 +1100

    split chunks

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,38 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/maverick-web'
+    : '/',
   
   configureWebpack: {
     plugins: [
     ],
-    externals: { ws: "WebSocket"},
+    externals: { ws: "WebSocket" },
+    output: {
+      filename: '[name].[hash].js',
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+  
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `yarn.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
+    },
   },
   css: {
     loaderOptions: {
